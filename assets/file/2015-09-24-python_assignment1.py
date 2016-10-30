18c39958136babfd60062c3991ad2f0da9d6706e
KyuHyuk Lee <lee@kyuhyuk.kr>
2016-10-30 20:27:04 +0900

    게시물 수정: '1 장: Hello World'

---
+++ assets/file/2015-09-24-python_assignment1.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+print '이름을 입력해주세요: '
+name = raw_input()
+
+print '전화번호를 입력해주세요: '
+phone = raw_input()
+
+print '이메일 주소를 입력해주세요: '
+email = raw_input()
+
+print '================================'
+print '이름: ' + name
+print '전화번호: ' + phone
+print '이메일 주소: ' + email
+print '================================'
---
+++ assets/file/2015-09-24-python_assignment2.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+print 'x = '
+x = input()
+x = float(x)
+
+print 'y = '
+y = input()
+y = float(y)
+
+print '================================'
+print str(x) + ' + ' + str(y) + ' = ' + str(x + y)
+print str(x) + ' - ' + str(y) + ' = ' + str(x - y)
+print str(x) + ' * ' + str(y) + ' = ' + str(x * y)
+print str(x) + ' / ' + str(y) + ' = ' + str(x / y)
+print '================================'
